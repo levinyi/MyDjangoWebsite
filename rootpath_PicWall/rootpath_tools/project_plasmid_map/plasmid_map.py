b193e10d948c930aff41dab1558a0b0a6c42d6f7
levin <dushiyi319@163.com>
2022-11-17 10:10:47 +0800

    some changes

--- rootpath_PicWall/rootpath_tools/project_plasmid_map/plasmid_map.py
+++ rootpath_PicWall/rootpath_tools/project_plasmid_map/plasmid_map.py
@@ -1,21 +1,40 @@
 import os
 import sys
+
 from Bio import SeqIO
 
-base_dir = os.path.dirname(os.path.abspath(__file__))
 
-AAV6_Kan_5End = os.path.join(base_dir, "AAV6_Kan_5End_Region.fa") 
-AAV6_Kan_3End = os.path.join(base_dir, "AAV6_Kan_3End_Region.fa")
-
-clonotype_file = sys.argv[1]
-output = sys.argv[2]
-
-End5 = [str(record.seq) for record in SeqIO.parse(AAV6_Kan_5End, "fasta")]
-End3 = [str(record.seq) for record in SeqIO.parse(AAV6_Kan_3End, "fasta")]
+def deal_selected_list(id_file):
+    f = open(id_file, "r")
+    selected_list = [i.rstrip("\n") for i in f]
+    f.close()
+    return selected_list
 
 
-for record in SeqIO.parse(clonotype_file, "fasta"):
-    record_id = str(record.id)
-    record_seq = End5[0]+str(record.seq)+End3[0]
-    with open(output+"/"+record_id+".fa", "w") as f:
-        f.write(">"+record_id+"\n"+record_seq+"\n")
+def main():
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+
+    AAV6_Kan_5End = os.path.join(base_dir, "AAV6_Kan_5End_Region.fa") 
+    AAV6_Kan_3End = os.path.join(base_dir, "AAV6_Kan_3End_Region.fa")
+
+    clonotype_file = sys.argv[1]
+    id_file = sys.argv[2]
+    output = sys.argv[3]  # output must be the last parmarater.
+    # print(id_file)
+
+    End5 = [str(record.seq) for record in SeqIO.parse(AAV6_Kan_5End, "fasta")]
+    End3 = [str(record.seq) for record in SeqIO.parse(AAV6_Kan_3End, "fasta")]
+
+    selected_list = deal_selected_list(id_file)
+    # print(selected_list)
+
+    for record in SeqIO.parse(clonotype_file, "fasta"):
+        record_id = str(record.id)
+        if record_id in selected_list:
+            record_seq = End5[0]+str(record.seq)+End3[0]
+            with open(output+"/"+record_id+".fa", "w") as f:
+                f.write(">"+record_id+"\n"+record_seq+"\n")
+
+
+if __name__ == '__main__':
+    main()
--- rootpath_PicWall/tools/views.py
+++ rootpath_PicWall/tools/views.py
@@ -1,8 +1,8 @@
 import os
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from rootpath_PicWall.settings import BASE_DIR
 from .models import Tools
 
 # Create your views here.
@@ -24,9 +24,7 @@ def tools_use(request, tools_name):
         # get submit times data and save to database model
         print("user_ip: {} has submit the value in {} tools".format(user_ip, tools_name))
         BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        result_path = BASE_DIR + \
-            "/rootpath_tools/project_{}/{}_results".format(
-                tools_name, tools_name)
+        result_path = BASE_DIR + "/rootpath_tools/project_{}/{}_results".format(tools_name, tools_name)
         print(tools_name)
         script_files, input_files = globals()[tools_name](request)
         python_script = "python3 {}/rootpath_tools/project_{}/{}.py {}".format(
@@ -34,16 +32,17 @@ def tools_use(request, tools_name):
         return save_file(request, result_path, python_script, input_files)
 
 
-def save_file(request, result_path, python_script, file_object_list):
-    '''定义一个函数, 用来保存用户上传的文件(可以有多个文件),且将文件传递给python脚本,并将结果打包成zip文件, 并发送给浏览器'''
+def save_file(request, result_path, python_script, input_file_list):
+    '''定义一个函数, 用来保存用户上传的文件(可以有多个文件),且将文件传递给python脚本,并将脚本执行后的结果打包成zip文件, 并发送给浏览器'''
     # 先删除result_path下的所有文件(是上一个用户运行的结果) bugs here: 用户没有上传文件，但点击了开始按钮，这时网页会报错，同时后台也会删除结果。
     os.system('rm -rf {}/*'.format(result_path))
     print("Remove: rm -rf {}/*".format(result_path))
 
-    file_list = []  # 文件顺序是怎么样的？按照用户上传的顺序？需要测试一下
-    for file_object in file_object_list:
+    '''保存用户上传的文件到本地'''
+    file_list = []  # 文件顺序是根据每个功能函数中append的顺序
+    for file_object in input_file_list:
         # 将用户上传的文件保存到 result_path目录下
-        file_name = file_object.name.replace(" ", "")  # 去掉文件名中的空格
+        file_name = file_object.name.replace(" ", "").replace('[',"").replace(']',"")  # 去掉文件名中的空格,[],
         # print("file_name: ", file_name) # 文件名, 没有路径, 只有文件名, 如: "test.txt"
         # 文件全路径, 如: "/rootpath_PicWall/tools/split_fasta_results/test.txt"
         file_path = os.path.join(result_path, file_name)
@@ -54,10 +53,12 @@ def save_file(request, result_path, python_script, file_object_list):
             for chunk in file_object.chunks():
                 f.write(chunk)
     print("Save files to result_path: {}".format(result_path))
+    
+    '''执行脚本进行文件处理'''
+    print('{} {} {}'.format(python_script, " ".join(file_list), result_path))
     os.system('{} {} {}'.format(python_script, " ".join(
         file_list), result_path))  # 调用python脚本
     # 等待系统执行分析脚本，完成后才能继续向下走：
-    print('{} {} {}'.format(python_script, " ".join(file_list), result_path))
     # 将result_path下的所有文件打包成zip文件
     result_name = result_path.split("/")[-1]
     # print("result_name : ", result_name)
@@ -73,16 +74,9 @@ def save_file(request, result_path, python_script, file_object_list):
         print("文件打包完成，已返回前端页面")
         return response
 
+#### 以下是执行各个工具的文件处理函数。
 
 def split_fasta(request):
-    """1 input file"""
-    script_files = ""
-    input_files = []
-    input_files.append(request.FILES.get("file_name"))
-    return script_files, input_files
-
-
-def plasmid_map(request):
     """1 input file"""
     script_files = ""
     input_files = []
@@ -133,4 +127,13 @@ def pyxls(request):
     input_files = []
     input_files.append(request.FILES.get("labRecord_file"))
     input_files.append(request.FILES.get("well_file"))
+    return script_files, input_files
+
+
+def plasmid_map(request):
+    """2 input files"""
+    script_files = ""
+    input_files = []
+    input_files.append(request.FILES.get("file_name"))
+    input_files.append(request.FILES.get("file_name2"))
     return script_files, input_files
