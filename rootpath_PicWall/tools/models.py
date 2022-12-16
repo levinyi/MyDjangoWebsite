ae3fdc3899045d83007f5a1a07893c747b9416cc
levin <dushiyi319@163.com>
2022-12-16 09:46:44 +0800

    some changes

--- rootpath_PicWall/tools/models.py
+++ rootpath_PicWall/tools/models.py
@@ -1,5 +1,5 @@
-from tabnanny import verbose
 from django.db import models
+
 
 # Create your models here.
 class Tools(models.Model):
@@ -7,4 +7,4 @@ class Tools(models.Model):
     tools_name = models.CharField(verbose_name="小工具", max_length=64)
     tools_desc = models.TextField(verbose_name="功能表述")
     tools_freq = models.SmallIntegerField(verbose_name="使用频率",)
-    tools_icon = models.ImageField(upload_to="images/tools_icon", blank=True)
+    tools_icon = models.ImageField(upload_to="static/img/tools_icon", blank=True)
